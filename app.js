1da22acc0541914c91a354b556c1f2680b346023
Harry <arandomawesomegeek@gmail.com>
2016-06-10 12:52:46 -0400

    make usable on GAE

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+'use strict';
+
 var stat = require('jsregress');
 var express = require('express');
 var r = require("nraw");
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello, world!');
+});
+
+var server = app.listen(process.env.PORT || '8080', function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
