97610acdd619952a467b55698e0ff49dae08e051
Harry <arandomawesomegeek@gmail.com>
2016-06-11 12:15:27 -0400

    IT WORKS

--- app.js
+++ app.js
@@ -2,22 +2,33 @@
 
 var stat = require('jsregress');
 var express = require('express');
-var r = require("nraw");
+var unirest = require('unirest');
+var http = require('http');
 
 var app = express();
-var Reddit = new r("Karma Helper");
 
 app.set('json spaces', 2);
 
-app.get('/?.json', function(req, res) {
+app.get('/*.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   var url = req.url;
-  url = url.substring(0, str.length - 5);
-  Reddit.subreddit(url).hot().exec(function(data){
-    
-  });
+  var redditResponse;
+  url = "http://www.reddit.com/r" + url.substring(0, url.length - 5) + "/hot.json?count=100";
+  var request = http.get(url, function(response) {
+    var json = '';
+    response.on('data', function(chunk) {
+      json += chunk;
+    });
 
-  res.json({ a: 1 });
+    response.on('end', function() {
+      redditResponse = JSON.parse(json);
+
+      res.send(redditResponse);
+    })
+  });
+  request.on('error', function(err) {
+    console.log(err);
+  });
 });
 
 var server = app.listen(process.env.PORT || '8080', function () {
