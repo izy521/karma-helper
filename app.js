5a92e56ee31b6476cffb8d4b68c3466350309d04
Harry <arandomawesomegeek@gmail.com>
2016-06-11 20:38:28 -0400

    added more stuff

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 (function() {
-var app = angular.module('karma-helper', []);
+  var app = angular.module('karma-helper', []);
 
-app.controller('KarmaHelperController', function() {
-  
-});
-
+  app.controller('KarmaHelperController', ['$http', function($http){
+    var sub = pcmasterrace;
+    this.getData(){
+      $http.get('https://karma-helper.herokuapp.com/' + sub + '.json').success(function(data){
+        returned = data;
+      });
+      
+    }
+  }]);
 })();
