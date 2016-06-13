5889682f4890f998a04cf29902343c1cb11ea082
Haoda Wang <arandomawesomegeek@gmail.com>
2016-06-13 11:09:48 -0400

    made it work!!!

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-(function() {
-  var app = angular.module('karma-helper', []);
+// Define the `phonecatApp` module
+var helper = angular.module('helper', []);
 
-  app.controller('KarmaHelperController', ['$http', function($http){
-    var sub = pcmasterrace;
-    this.getData(){
-      $http.get('https://karma-helper.herokuapp.com/' + sub + '.json').success(function(data){
-        returned = data;
-      });
-      
-    }
-  }]);
-})();
+// Define the `PhoneListController` controller on the `phonecatApp` module
+helper.controller('HelperController', ['$http', '$scope', function($http, $scope) {
+  $scope.subreddit = "pcmasterrace";
+  $scope.results = {};
+  $http.get('https://karma-helper.herokuapp.com/' + $scope.subreddit + '.json').success(function(data){
+    $scope.results = data;
+  });
+}]);
