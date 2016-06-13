f1267054b72109cb985a3f31aabdbb165e980ab0
Haoda Wang <arandomawesomegeek@gmail.com>
2016-06-13 11:23:59 -0400

    switch to materialize

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var helper = angular.module('helper', []);
 // Define the `PhoneListController` controller on the `phonecatApp` module
 helper.controller('HelperController', ['$http', '$scope', function($http, $scope) {
   $scope.subreddit = "pcmasterrace";
-  $scope.results = {};
+  $scope.results = [];
+  $scope.bestTime = 0;
+  $scope.bestScore = 0;
   $http.get('https://karma-helper.herokuapp.com/' + $scope.subreddit + '.json').success(function(data){
-    $scope.results = data;
+    $scope.results = data.results;
+    $scope.bestTime = data.best;
+    $scope.bestScore = data.bestScore;
   });
 }]);
