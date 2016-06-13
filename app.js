f39fb1bec294e36c442ee44d9e295378e0abfa7e
veggiedefender <jessejesse123@gmail.com>
2016-06-13 11:42:14 -0400

    added form and a thing

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var helper = angular.module('helper', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 helper.controller('HelperController', ['$http', '$scope', function($http, $scope) {
-  $scope.subreddit = "pcmasterrace";
+  $scope.subreddit;
   $scope.results = [];
   $scope.bestTime = 0;
   $scope.bestScore = 0;
-  $http.get('https://karma-helper.herokuapp.com/' + $scope.subreddit + '.json').success(function(data){
-    $scope.results = data.result;
-    $scope.bestTime = data.best;
-    $scope.bestScore = data.bestScore;
-  });
+
+  $scope.getResults = function() {
+    $http.get('https://karma-helper.herokuapp.com/' + $scope.subreddit + '.json').success(function(data){
+        $scope.results = data.result;
+        $scope.bestTime = data.best;
+        $scope.bestScore = data.bestScore;
+      });
+    }
 }]);
